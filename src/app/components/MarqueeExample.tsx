aff956affba9e5bdda3777b25b4acba5061baf07
Arjohn Ely <arjohn.ely45@gmail.com>
2024-12-22 12:13:06 +0800

    updates

--- src/app/components/MarqueeExample.tsx
+++ src/app/components/MarqueeExample.tsx
@@ -1,54 +1,50 @@
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 import Marquee from "@/components/ui/marquee";
 import "./marquee-example.css";
 
 const reviews = [
   {
     name: "Sparc",
-    type: "video",
     body: "Effortless rent payments within your apartment building. All maintenance requests are just one click away. Sparc integrates resident events, fitness classes, and community conversations. Whether it's dog walking, massage therapy, or any other custom service, Sparc makes it easy to book and enjoy personalized care, tailored to your lifestyle-all from the convenience of your phone.",
-    link: "/videos/new_sparc.mp4",
+    link: "/img/sparc.gif",
     color: "from-yellow-300",
     appType: "Mobile App",
   },
   {
     name: "JoltPay",
-    type: "image",
     body: "Jolt Pay is a game-changing point of sale (POS) software, designed to simplify taking orders and payments for mobile food and goods services. As a progressive point of sales software, Jolt Pay redefines the ordering and purchasing experience, enhancing your customer engagement and driving your business growth. With the power of pre-ordering, location services, loyalty programs, push notifications, and detailed analytics, you gain the tools to forecast and scale your business growth effectively.",
     link: "/img/joltpay.gif",
     color: "from-sky-400",
     appType: "PWA App",
   },
   {
-    name: "Xvend",
-    type: "image",
-    body: "Jolt Pay is a game-changing point of sale (POS) software, designed to simplify taking orders and payments for mobile food and goods services. As a progressive point of sales software, Jolt Pay redefines the ordering and purchasing experience, enhancing your customer engagement and driving your business growth. With the power of pre-ordering, location services, loyalty programs, push notifications, and detailed analytics, you gain the tools to forecast and scale your business growth effectively.",
-    link: "/img/joltpay.gif",
-    color: "from-yellow-300",
-    appType: "Mobile App",
-  },
-  {
     name: "HyperSpace",
     type: "image",
-    body: "Jolt Pay is a game-changing point of sale (POS) software, designed to simplify taking orders and payments for mobile food and goods services. As a progressive point of sales software, Jolt Pay redefines the ordering and purchasing experience, enhancing your customer engagement and driving your business growth. With the power of pre-ordering, location services, loyalty programs, push notifications, and detailed analytics, you gain the tools to forecast and scale your business growth effectively.",
-    link: "/img/joltpay.gif",
+    body: "If you have a space, garage, or storage unit that has not been used for months or years, publish it and start earning. The Hyperspace mobile app lets you publish your space by uploading space images and inputting locations and size details. It has a notification feature for both renters and owners. They can also create conversations with the app messaging feature. Renters can check which space is nearby by using the app map feature.",
+    link: "/img/hyperspace.gif",
     color: "from-blue-900",
     appType: "Mobile App",
   },
   {
     name: "Asean",
-    type: "video",
-    body: "Jolt Pay is a game-changing point of sale (POS) software, designed to simplify taking orders and payments for mobile food and goods services. As a progressive point of sales software, Jolt Pay redefines the ordering and purchasing experience, enhancing your customer engagement and driving your business growth. With the power of pre-ordering, location services, loyalty programs, push notifications, and detailed analytics, you gain the tools to forecast and scale your business growth effectively.",
-    link: "/videos/asean.mp4",
+    body: "Booking was made easy. The ASEAN PWA app was used during the AUSTRALIA-ASEAN BUSINESS FORUM. Users can register with the app by scanning the QR code presented to them, inputting the email, and verifying the code. Attendees can book with each country’s delegates and representatives using QR code scanning, while delegates can be notified of the attendees and booking details.",
+    link: "/img/asean.gif",
     color: "from-sky-950",
     appType: "PWA App",
   },
   {
     name: "News Network",
-    type: "video",
-    body: "Jolt Pay is a game-changing point of sale (POS) software, designed to simplify taking orders and payments for mobile food and goods services. As a progressive point of sales software, Jolt Pay redefines the ordering and purchasing experience, enhancing your customer engagement and driving your business growth. With the power of pre-ordering, location services, loyalty programs, push notifications, and detailed analytics, you gain the tools to forecast and scale your business growth effectively.",
-    link: "/videos/asean.mp4",
+    body: "Want to increase your website ranking? The news network is the best website for you. The news network web app lets you create an article, embed your website link, and post it to more than 1,000 websites available. You can select 400 different categories where you find your website link fits. It is a subscription-based monthly membership with flexible plans.",
+    link: "https://seodirectservice.com/",
     color: "from-blue-700",
+    appType: "Web App",
+  },
+  {
+    name: "Oasis Dentail Call Center LLC",
+    body: "The Oasis Dental Call Center is currently in its inception phase, having been conceived through the collaboration of the Gabucan brothers. Its primary goal is to elevate the overall patient experience within the realm of dental practice, laying the groundwork for synergistic and innovative solutions. The first step taken in this direction was streamlining the initial phase of a patient’s visit by providing relevant, real-time insurance benefits information, thus enabling an informed and collaborative approach toward achieving the patients’ goals.",
+    link: "https://www.oasisdentalcallcenter.com/",
+    color: "from-sky-500",
     appType: "Web App",
   },
 ];
@@ -69,16 +65,16 @@ export function MarqueeDemo() {
 
 const ReviewCard = ({
   name,
-  type,
   body,
   link,
   color,
+  appType
 }: {
   name: string;
-  type: string;
   body: string;
   link: string;
   color: string;
+  appType: string
 }) => {
   return (
     <figure
@@ -92,34 +88,25 @@ const ReviewCard = ({
           `bg-gradient-to-br ${color} to-transparent`
         )}
       >
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center h-full">
           <div className="flex flex-col w-full">
             <h1 className="text-black text-center font-mono text-[40px]">
               {name}
             </h1>
-            <h1 className="text-black text-center font-mono">Mobile App</h1>
+            <h1 className="text-black text-center font-mono">{appType}</h1>
             <div>
               <h1 className="text-black p-5 text-ellipsis max-h-[300px] h-[300px] overflow-scroll overflow-x-clip">{body}</h1>
             </div>
           </div>
-          {type === "image" ? (
+          {appType === "Web App" ? <div className="flex justify-center w-[400px]">
+            <Link href={link} target="_blank" className="text-center text-black">Visit website</Link>
+          </div> :
             <img
               src={link}
               alt={`${name} GIF`}
-              className="place-self-start h-[450px] w-[400px] p-5 rounded-md"
+              className="flex justify-center h-[450px] w-[400px] p-5 rounded-md"
             ></img>
-          ) : (
-            <video
-              className="place-self-start h-[450px] w-[300px] p-5 rounded-md"
-              controls
-              autoPlay
-              loop
-              muted
-            >
-              <source src={link} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          )}
+          }
         </div>
       </div>
     </figure>
